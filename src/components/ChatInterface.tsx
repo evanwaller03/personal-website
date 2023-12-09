c07f2401ac6d8408bb8a2c57aa7bd92f5511ec2a
Evan Waller <evanwaller03@gmail.com>
2023-12-08 21:24:55 -0600

    try this

--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -15,7 +15,7 @@ const ChatInterface = () => {
     const data = { prompt: question };
     
     try {
-      const response = await fetch('http://127.0.0.1:5000/ask', {
+      const response = await fetch('http://127.0.0.1:8000/ask', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
