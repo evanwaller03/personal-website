f984cf6970fd6d66a8851f75eb2d3fe1eef12870
Evan Waller <evanwaller03@gmail.com>
2023-12-10 23:41:35 -0600

    pleaseeee

--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -15,7 +15,7 @@ const ChatInterface = () => {
     const data = { prompt: question };
     
     try {
-      const response = await fetch('http://34.28.122.14:8000/ask', {
+      const response = await fetch('https://api.evanwaller.com/ask', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -44,7 +44,7 @@ const ChatInterface = () => {
 
   const testCORS = async () => {
     try {
-      const response = await fetch('http://34.28.122.14:8000/test-cors', {
+      const response = await fetch('https://api.evanwaller.com/test-cors', {
         method: 'GET'
       });
   
