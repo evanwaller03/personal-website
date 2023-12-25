7ea28f7476fa364fef962b5e37f0e7ce561557a0
Evan Waller <evanwaller03@gmail.com>
2023-12-25 14:32:49 -0500

    les go

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, CSSProperties } from 'react';
+import React, { useState, CSSProperties, useEffect } from 'react';
 import Logo from '../../public/w-logo-square.png';
 import { HiMenu } from "react-icons/hi";
 import Dropdown from 'react-bootstrap/Dropdown';
@@ -12,6 +12,26 @@ const NavBar = ({ title } : any) => {
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
+
+  useEffect(() => {
+    // Handler to close the dropdown if the click is outside
+    const closeDropdown = (event: any) => {
+      const dropdownMenuElement = document.getElementById('dropdownMenuElement');
+      const dropdownButtonElement = document.getElementById('dropdownButtonElement');
+  
+      // Check if the click was outside of these elements
+      if (dropdownMenuElement && !dropdownMenuElement.contains(event.target) &&
+          dropdownButtonElement && !dropdownButtonElement.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    };
+  
+    // Add event listener for clicks
+    document.addEventListener('click', closeDropdown);
+  
+    // Remove event listener on cleanup
+    return () => document.removeEventListener('click', closeDropdown);
+  }, [isDropdownOpen]); // Only re-run if isDropdownOpen changes
    
   const circleStyle = {
     width: '60px',
@@ -44,12 +64,18 @@ const NavBar = ({ title } : any) => {
     position: 'absolute',
     backgroundColor: '#343541',
     minWidth: '160px',
-    boxShadow: '0px 8px 16px 0px rgba(0,0,0,0.4)',
+    boxShadow: '0 0 10px white',
     padding: '12px 16px',
     borderRadius: '7px',
     zIndex: 20000,
+    border: '2px solid white',
     right: 0, // Adjust this as needed
     top: '80px', // Position below the navbar
+  };
+
+  const dropDownItem: CSSProperties = {
+    fontSize: '1.1em',
+    marginBottom: '7px',
   };
 
   const handleNavigation = (path: string) => {
@@ -84,19 +110,19 @@ const NavBar = ({ title } : any) => {
         onMouseLeave={(e) => {
             e.currentTarget.style.border = '2px solid #272831';
             e.currentTarget.style.boxShadow = 'none';
-        }}>
+        }} 
+        id="dropdownButtonElement">
         <HiMenu style={{fontSize: '30'}}/>
-        <div style={dropdownContent}>
+        <div style={dropdownContent} id="dropdownButtonElement" >
           {/* Add navigation items here */}
-          <div onClick={() => handleNavigation('/')}>Welcome</div>
-          <div onClick={() => handleNavigation('/resume')}>Resume</div>
-          <div onClick={() => handleNavigation('/mycode')}>My Code</div>
-          <div onClick={() => handleNavigation('/youtube')}>YouTube</div>
+          <div style={dropDownItem} onClick={() => handleNavigation('/')}>Welcome</div>
+          <div style={dropDownItem} onClick={() => handleNavigation('/resume')}>Resume</div>
+          <div style={dropDownItem} onClick={() => handleNavigation('/mycode')}>My Code</div>
+          <div style={dropDownItem} onClick={() => handleNavigation('/youtube')}>YouTube</div>
           {/* <div onClick={() => handleNavigation('/galaxyinvaders')}>Galaxy Invaders</div> */}
-          <div onClick={() => handleNavigation('/wallergpt')}>WallerGPT</div>
+          <div style={dropDownItem} onClick={() => handleNavigation('/wallergpt')}>WallerGPT</div>
           {/* <div onClick={() => handleNavigation('/quotes')}>Quotes</div> */}
           {/* <div onClick={() => handleNavigation('/interests')}>Interests</div> */}
-          {/* Add more navigation items as needed */}
         </div>
       </div>
     </div>
