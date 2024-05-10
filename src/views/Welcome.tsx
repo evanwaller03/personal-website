39b8c955588ac9c9a0d6c7127faa4011b4a7f8fb
Evan Waller <evanwaller03@gmail.com>
2024-05-10 13:37:08 -0500

    updates to home page

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -51,25 +51,28 @@ const Welcome = () => {
         <div style={{ position: 'relative', width: '100vw', height: '100vh', overflow: 'auto', paddingBottom: '25px'}}>
             <div className="moving-background"></div> {/* Separate div for moving background */}
             <div className='navbar-div'>
-                <NavBar title="Wally's World"/>
+                <NavBar title={isMobile ? "Wally's World" : "Welcome to Wally's World"}/>
             </div>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', marginTop: '80px', overflow: 'scroll'}}>
-                <h1 style={titleStyle}>Welcome to Wally's World</h1>
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', marginTop: '80px', overflow: 'scroll', height:'80%'}}>
+                {/* <h1 style={titleStyle}>Welcome to Wally's World</h1> */}
                 <div style={pageStyle}>   
                     <div style={{ border:'1px solid #d3d3d3', borderRadius: '4px', padding: '5px'}}>
                         <h3 style={{ textAlign: 'center', marginBottom: '15px', fontSize: '1.2em', textDecoration: 'underline' }}>Quick Statistics:</h3>
                         <ul style={{ listStyleType: 'none', padding: '0', textAlign: 'left' }}>
                             <li><strong>Name:</strong> Evan Waller</li>
                             <li><strong>Education:</strong> Gies College of Business, University of Illinois</li>
-                            <li><strong>Degree:</strong> Bachelor of Science in <span style={{ color: '#22bcf3' }}>Finance</span></li>
-                            <li><strong>YouTube University:</strong> <span style={{ color: '#22bcf3' }}>Computer Science</span> since June 2021</li>
+                            <li><strong>Degree:</strong> Bachelor of Science in <span style={{ fontWeight: 'bold',color: '#22bcf3' }}>Finance</span></li>
+                            <li><strong>Other Interests:</strong> <span style={{ fontWeight: 'bold',color: '#22bcf3' }}>Computer Science</span> since June 2021</li>
+                            {/* <a className='hover-underline' href="https://chat.openai.com/g/g-cN5P2jtNn-senior-technology-guide" target="_blank" rel="noopener noreferrer">
+                                ChatGPT Store Project: Senior Technology Guide
+                            </a> */}
                         </ul>
                         <strong>Questions?</strong> Ask <Link className='hover-underline' style={{ fontWeight: 'bold', textDecoration: isMobile ? 'underline': '', display: 'inline' }} to="/wallergpt">WallerGPT: my openAI fine-tuned chatbot</Link>
                     </div>
-                    <Link className='hover-underline' style={{marginTop: '50px', marginBottom: '15px'}} to="/resume"><strong style={{fontSize: isMobile ? '1.1em' : '1.8em', textDecoration: isMobile ? 'underline': ''}}>📄 Resume:</strong> Education & Professional Work Experience</Link>
-                    <Link className='hover-underline' style={{marginBottom: '15px'}} to="/mycode"><strong style={{fontSize: isMobile ? '1.1em' : '1.8em', textDecoration: isMobile ? 'underline': ''}}>💻 My Code:</strong> What I Have Built</Link>
-                    <Link className='hover-underline' style={{marginBottom: '15px',}} to="/wallergpt"><strong style={{fontSize: isMobile ? '1.1em' : '1.8em', textDecoration: isMobile ? 'underline': ''}}>🤖 WallerGPT:</strong> My AI Clone (In Progress)</Link>
-                    <Link className='hover-underline' style={{marginBottom: '15px'}}to="/youtube"><strong style={{fontSize: isMobile ? '1.1em' : '1.8em', textDecoration: isMobile ? 'underline': ''}}>🚀 Anding Analytics - YouTube:</strong> My YouTube Channel</Link>
+                    <Link className='hover-underline' style={{marginTop: '50px', marginBottom: '15px'}} to="/resume"><strong style={{fontSize: isMobile ? '1.1em' : '1.3em', textDecoration: isMobile ? 'underline': ''}}>📄 Resume:</strong> Education & Professional Work Experience</Link>
+                    <Link className='hover-underline' style={{marginBottom: '15px'}} to="/mycode"><strong style={{fontSize: isMobile ? '1.1em' : '1.3em', textDecoration: isMobile ? 'underline': ''}}>💻 My Code:</strong> What I Have Built</Link>
+                    <Link className='hover-underline' style={{marginBottom: '15px',}} to="/wallergpt"><strong style={{fontSize: isMobile ? '1.1em' : '1.3em', textDecoration: isMobile ? 'underline': ''}}>🤖 WallerGPT:</strong> My AI Clone (In Progress)</Link>
+                    <Link className='hover-underline' style={{marginBottom: '15px'}}to="/youtube"><strong style={{fontSize: isMobile ? '1.1em' : '1.3em', textDecoration: isMobile ? 'underline': ''}}>🚀 Anding Analytics - YouTube:</strong> My YouTube Channel</Link>
                     {/* <Link className='hover-underline' style={{marginTop: '15px'}} to="/galaxyinvaders">🕹️ <strong style={{fontSize: isMobile ? '1.1em' : '1.8em', textDecoration: isMobile ? 'underline': ''}}>Galaxy Invaders:</strong> A Tribute To My First Python Creation</Link> */}
                     {/* <Link className='hover-underline' style={{marginBottom: '50px',}} to="/interests"><strong style={{fontSize: isMobile ? '1.1em' : '1.8em', textDecoration: isMobile ? 'underline': ''}}>🌌 Interests:</strong> What I Do For Fun</Link> */}
                     <p style={{fontSize:'13px'}}>Disclaimer: The website will forever be a work in progress. Contact me if you have any cool ideas or would like to report a bug. Email: evanwaller03@gmail.com</p>
